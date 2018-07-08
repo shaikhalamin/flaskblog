626fbbd585e9c77cdfc866bb126f504266a15821
Shaikh <samin@teamamericany.com>
2018-07-08 15:22:31 +0600

    create post added

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -98,10 +98,27 @@ def new_post():
 		db.session.commit()
 		flash('Post has been created','info')
 		return redirect(url_for('home'))
-	return render_template('create_post.html',title='Create post',form=form)
+	return render_template('create_post.html',title='Create post',form=form,legend='Create post')
 
 @app.route('/post/<int:post_id>')
 def post(post_id):
-	post = Post.query.get(post_id)
-	#return render_template('')
+	post = Post.query.get_or_404(post_id)
+	return render_template('post.html',title=post.title,post=post)
 
+@app.route('/post/<int:post_id>/update', methods=['GET','POST'])
+@login_required
+def update_post(post_id):
+	post = Post.query.get_or_404(post_id)
+	if post.author != current_user:
+		abort(403)
+	form = PostForm()
+	if form.validate_on_submit():
+		post.title = form.title.data
+		post.content = form.content.data
+		db.session.commit()
+		flash('Post has been updated','info')
+		return redirect(url_for('post',post_id=post.id))
+	elif request.method == 'GET':
+		form.title.data = post.title
+		form.content.data = post.content
+	return render_template('create_post.html',title=post.title,post=post,form=form,legend='Update post')
